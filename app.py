3deb0a4580fada4de2f60c06e33830d0718569ed
St@rs;ArcLIght <128189013+bun1144@users.noreply.github.com>
2024-03-08 14:22:29 +0000

    first

--- app.py
+++ app.py
@@ -1,7 +1,72 @@
-from flask import Flask, render_template
+from flask import request,Flask,jsonify
+from flask_cors import CORS,cross_origin
 
-app = Flask(__name__)
+app = Flask(__name__) 
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
+books=[
+    {"id":1,"title":"Book500","author":"Author 1"},
+    {"id":2,"title":"Book 2","author":"Author 2"},
+    {"id":3,"title":"Book 3","author":"Author 3"}
+]
 @app.route("/")
-def hello_world():
-    return render_template("index.html", title="Hello")
+def Greet():
+    return "<p>Welcome to Book Management Systems</p>"
+
+@app.route("/books",methods=["GET"])
+@cross_origin()
+def get_all_books():
+    return jsonify({"books":books})
+
+@app.route("/books/<int:book_id>",methods=["GET"])
+@cross_origin()
+def get_book(book_id):
+    book =  next(( b for b in books if b["id"]==book_id ),None)
+    if book:
+        return jsonify(book)
+    else:
+        return jsonify({"error":"Book not found"}),404
+
+@app.route("/books",methods=["POST"])
+@cross_origin()
+def create_book():
+    data = request.get_json()
+    new_book={
+        "id":len(books)+1,
+        "title":data["title"],
+        "author":data["author"]
+    }
+    books.append(new_book)
+    return jsonify(new_book),201
+
+@app.route("/books/<int:book_id>",methods=["PUT"])
+@cross_origin()
+def update_book(book_id):
+    book = next((b for b in books if b["id"]==book_id),None)
+    if book:
+        data = request.get_json()
+        book.update(data)
+        return jsonify(book)
+    else:
+        return jsonify({"error":"Book not found"}),404
+
+
+
+
+@app.route("/books/<int:book_id>",methods=["DELETE"])
+@cross_origin()
+def delete_book(book_id):
+    book = next((b for b in books if b["id"]==book_id),None)
+    if book:
+        books.remove(book)
+        return jsonify({"message":"Book deleted successfully"}),200
+    else:
+        return jsonify({"error":"Book not found"}),404
+    
+
+
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0",port=5000,debug=True)
